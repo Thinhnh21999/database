42a02d1b18ea7e2a5c2d8eb803429f35498415b4
Dungnq <nguyen.quoc.dung@sun-asterisk.com>
2023-05-11 22:28:31 +0700

    server authen

--- server.js
+++ server.js
@@ -1,123 +1,161 @@
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const jsonServer = require('json-server')
-const jwt = require('jsonwebtoken')
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
+const queryString = require("query-string");
 
-const server = jsonServer.create()
-const router = jsonServer.router('./database.json')
-const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+const server = jsonServer.create();
+const router = jsonServer.router("./database.json");
+const userdb = JSON.parse(fs.readFileSync("./users.json", "UTF-8"));
 
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 server.use(jsonServer.defaults());
 
-const SECRET_KEY = '123456789'
+const SECRET_KEY = "123456789";
 
-const expiresIn = '1h'
+// list api not have check auth
+const API_NOT_AUTH = ["/products"];
 
-// Create a token from a payload 
-function createToken(payload){
-  return jwt.sign(payload, SECRET_KEY, {expiresIn})
+const expiresIn = "1h";
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
-// Verify the token 
-function verifyToken(token){
-  return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
 }
 
 // Check if the user exists in database
-function isAuthenticated({email, password}){
-  return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+function isAuthenticated({ email, password }) {
+  return (
+    userdb.users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) !== -1
+  );
 }
 
 // Register New User
-server.post('/auth/register', (req, res) => {
+server.post("/auth/register", (req, res) => {
   console.log("register endpoint called; request body:");
   console.log(req.body);
-  const {email, password} = req.body;
+  const { email, password, ...rest } = req.body;
 
-  if(isAuthenticated({email, password}) === true) {
+  if (isAuthenticated({ email, password }) === true) {
     const status = 401;
-    const message = 'Email and Password already exist';
-    res.status(status).json({status, message});
-    return
+    const message = "Email and Password already exist";
+    res.status(status).json({ status, message });
+    return;
   }
 
-fs.readFile("./users.json", (err, data) => {  
+  fs.readFile("./users.json", (err, data) => {
     if (err) {
-      const status = 401
-      const message = err
-      res.status(status).json({status, message})
-      return
-    };
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message });
+      return;
+    }
 
     // Get current users data
-    var data = JSON.parse(data.toString());
+    var data = JSON.parse(data?.toString());
 
     // Get the id of last user
-    var last_item_id = data.users[data.users.length-1].id;
+    var last_item_id = data.users[data.users.length - 1]?.id;
 
     //Add new user
-    data.users.push({id: last_item_id + 1, email: email, password: password}); //add some data
-    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
+    data.users.push({ id: last_item_id + 1, email: email, password: password, ...rest }); //add some data
+    var writeData = fs.writeFile(
+      "./users.json",
+      JSON.stringify(data),
+      (err, result) => {
+        // WRITE
         if (err) {
-          const status = 401
-          const message = err
-          res.status(status).json({status, message})
-          return
+          const status = 401;
+          const message = err;
+          res.status(status).json({ status, message });
+          return;
         }
-    });
+      }
+    );
+  });
+
+  // Create token for new user
+  const access_token = createToken({ email, password });
+  console.log("Access Token:" + access_token);
+  res.status(200).json({ access_token });
 });
 
-// Create token for new user
-  const access_token = createToken({email, password})
-  console.log("Access Token:" + access_token);
-  res.status(200).json({access_token})
-})
-
 // Login to one of the users from ./users.json
-server.post('/auth/login', (req, res) => {
+server.post("/auth/login", (req, res) => {
   console.log("login endpoint called; request body:");
   console.log(req.body);
-  const {email, password} = req.body;
-  if (isAuthenticated({email, password}) === false) {
-    const status = 401
-    const message = 'Incorrect email or password'
-    res.status(status).json({status, message})
-    return
+  const { email, password } = req.body;
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401;
+    const message = "Incorrect email or password";
+    res.status(status).json({ status, message });
+    return;
   }
-  const access_token = createToken({email, password})
+  const access_token = createToken({ email, password });
   console.log("Access Token:" + access_token);
-  res.status(200).json({access_token})
-})
+  res.status(200).json({ access_token, email });
+});
 
-server.use(/^(?!\/auth).*$/,  (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({status, message})
-    return
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  const isApiAuth = !API_NOT_AUTH.includes(req?.baseUrl);
+  if (
+    (req.headers.authorization === undefined ||
+      req.headers.authorization.split(" ")[0] !== "Bearer") &&
+    isApiAuth
+  ) {
+    const status = 401;
+    const message = "Error in authorization format";
+    res.status(status).json({ status, message });
+    return;
   }
   try {
     let verifyTokenResult;
-     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+    verifyTokenResult = verifyToken(req?.headers?.authorization?.split(" ")[1]);
 
-     if (verifyTokenResult instanceof Error) {
-       const status = 401
-       const message = 'Access token not provided'
-       res.status(status).json({status, message})
-       return
-     }
-     next()
+    if (verifyTokenResult instanceof Error && isApiAuth) {
+      const status = 401;
+      const message = "Access token not provided";
+      res.status(status).json({ status, message });
+      return;
+    }
+    next();
   } catch (err) {
-    const status = 401
-    const message = 'Error access_token is revoked'
-    res.status(status).json({status, message})
+    const status = 401;
+    const message = "Error access_token is revoked";
+    res.status(status).json({ status, message });
   }
-})
+});
 
-server.use(router)
+router.render = (req, res) => {
+  const headers = res.getHeaders();
+  const totalCountHeader = headers["x-total-count"];
+  if (req.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedOriginalUrl.query);
+    const results = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+    return res.jsonp(results);
+  }
+  return res.jsonp(res.locals.data);
+};
+
+server.use(router);
 
 server.listen(8000, () => {
-  console.log('Run Auth API Server')
-})
+  console.log("Run Auth API Server");
+});
